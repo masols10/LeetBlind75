47330741a0a3087cfe197485372faed08a3ce0d1
masols <manasa.solasa10@gmail.com>
2022-07-20 18:29:09 -0700

    Time: 2118 ms (31.22%), Space: 29 MB (20.39%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -5,9 +5,9 @@ class Solution:
         
         for n in nums:
             # check if its the start of a sequence
-            if (n - 1) not in numSet:
+            if (n - 1) not in numSet: #checks if consecutive left element is present or not
                 length = 1
-                while (n + length) in numSet:
+                while (n + length) in numSet: #checks for consecutive elements along the len
                     length += 1
                 longest = max(length, longest)
         return longest
