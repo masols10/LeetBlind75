f3b295e024167e2ea28f4a96a5660745bd1ba197
masols <manasa.solasa10@gmail.com>
2022-07-25 14:58:46 -0700

    Time: 113 ms (12.86%), Space: 14.4 MB (85.64%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # T- O(n), S- O(n)
+        '''filtered_chars = filter(lambda ch: ch.isalnum(), s)
+        lowercase_filtered_chars = map(lambda ch: ch.lower(), filtered_chars)
+
+        filtered_chars_list = list(lowercase_filtered_chars)
+        reversed_chars_list = filtered_chars_list[::-1]
+
+        return filtered_chars_list == reversed_chars_list '''
+    
+        # Two pointer approach 
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphanum(s[l]):
+                l += 1
+            while l < r and not self.alphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+    # Could write own alpha-numeric function
+    def alphanum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
+
+
+
+
