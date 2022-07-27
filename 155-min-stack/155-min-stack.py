93ffa68a5b03c61419085346f56799095489f87c
masols <manasa.solasa10@gmail.com>
2022-07-26 17:02:29 -0700

    Time: 56 ms (98.37%), Space: 18.1 MB (36.93%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,6 +1,6 @@
-from collections import deque
+'''from collections import deque
 class MinStack:
-
+    
     def __init__(self):
         self.stack = deque() 
 #Deque is a doubly linked list while List is just an array. Randomly accessing an object at index i is O(n) for Deque but O(1) for List . Fast insertions and deletions at the beginning is the biggest advantage of Deque . Fast random reads is the advantage of List .
@@ -20,7 +20,7 @@ class MinStack:
         return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.stack[-1][1]
+        return self.stack[-1][1] '''
 
 
 # Your MinStack object will be instantiated and called as such:
@@ -29,3 +29,54 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+# must implement a solution with O(1) time complexity for each function so maintain minstack
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
