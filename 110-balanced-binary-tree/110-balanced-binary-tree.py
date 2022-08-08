9232e7dcac4a2733e80b92f869ec67fca1e5305e
masols <manasa.solasa10@gmail.com>
2022-08-08 16:27:52 -0700

    Time: 74 ms (65.88%), Space: 19.1 MB (11.40%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+        # Top-down recursion - T- O(nlogn), S- O(n)
+        # Compute the tree's height via recursion
+    def height(self, root: TreeNode) -> int:
+        # An empty tree has height -1
+        if not root:
+            return -1
+        return 1 + max(self.height(root.left), self.height(root.right))
+    
+    def isBalanced(self, root: TreeNode) -> bool:
+        # An empty tree satisfies the definition of a balanced tree
+        if not root:
+            return True
+
+        # Check if subtrees have height within 1. If they do, check if the
+        # subtrees are balanced
+        return abs(self.height(root.left) - self.height(root.right)) < 2 \
+            and self.isBalanced(root.left) \
+            and self.isBalanced(root.right)
+    
+        # Bottom-up recursion - T-O(n), S- O(n)
+        # Return whether or not the tree at root is balanced while also returning
+    # the tree's height
+    def isBalancedHelper(self, root: TreeNode) -> (bool, int):
+        # An empty tree is balanced and has height -1
+        if not root:
+            return True, -1
+        
+        # Check subtrees to see if they are balanced. 
+        leftIsBalanced, leftHeight = self.isBalancedHelper(root.left)
+        if not leftIsBalanced:
+            return False, 0
+        rightIsBalanced, rightHeight = self.isBalancedHelper(root.right)
+        if not rightIsBalanced:
+            return False, 0
+        
+        # If the subtrees are balanced, check if the current tree is balanced
+        # using their height
+        return (abs(leftHeight - rightHeight) < 2), 1 + max(leftHeight, rightHeight)
+        
+    def isBalanced(self, root: TreeNode) -> bool:
+        return self.isBalancedHelper(root)[0]
