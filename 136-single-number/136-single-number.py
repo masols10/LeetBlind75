940a4a0925f473a071722aba28c4e7e7264c3253
masols <manasa.solasa10@gmail.com>
2022-10-06 19:00:32 -0700

    Update 136-single-number.py

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,7 +1,7 @@
     
 from collections import defaultdict
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+    def singleNumber(self, nums: List[int]) -> int: # Time complexity and space complexity will be O(N)
         hash_table = defaultdict(int)
         for i in nums:
             hash_table[i] += 1
@@ -10,7 +10,7 @@ class Solution:
             if hash_table[i] == 1:
                 return i
             
-    # Bit manipulation solution
+    # Bit manipulation solution - Complexity will be linear for both time and space
         res = 0
             for num in nums:
                 res = res ^ num #XOR of same number results in 0 and the remaining number will be the single number
