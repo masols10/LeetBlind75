a16c16eae8ddc0609e2a561fefe965109771900f
masols <manasa.solasa10@gmail.com>
2022-10-08 11:15:33 -0700

    Update 252-meeting-rooms.py

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -2,7 +2,7 @@ class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         # Time complexity - O(nlogn) and space complexity - O(1)
         intervals.sort()
-        for i in range(len(intervals) - 1):
-            if intervals[i][1] > intervals[i + 1][0]:
+        for i in range(len(intervals) - 1): # iterate starting from index 1
+            if intervals[i][1] > intervals[i + 1][0]: # when start time of current interval is less than the end time of previous interval they overalap
                 return False
         return True
