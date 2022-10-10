e04fa83923748b1127e486ff2e85182d9a115361
masols <manasa.solasa10@gmail.com>
2022-10-09 18:55:13 -0700

    Time: 1679 ms, Memory: 75.1 MB - LeetHub

---
+++ lru-cache/lru-cache.py
@@ -0,0 +1,51 @@
+class LRUCache:
+
+    # using dictionary
+    def __init__(self, capacity: int):
+        self.cap=capacity
+        self.data={}
+
+    def get(self, key: int) -> int:
+        if key not in self.data: return -1
+		#item recently accessed need to be sent at last of dict, pop and reassign 
+        self.data[key]=self.data.pop(key)
+        return self.data[key]
+    
+    def put(self, key: int, value: int) -> None:
+        if key in self.data:
+			#item updated recently need to be sent at last of dict, pop and update
+            self.data.pop(key)
+            self.data[key]=value
+            return
+		#if capacity is reached, we removed first item of dict. which is always least used
+        if self.cap==len(self.data): self.data.pop(next(iter(self.data)))
+        self.data[key]=value
+        return
+
+    # using an ordered dict
+    # Time complexity : O(1) both for put and get since all operations with ordered dictionary
+    #Space complexity : O(capacity) since the space is used only for an ordered dictionary with at most capacity + 1 elements
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = collections.OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.cache) < self.capacity or key in self.cache:
+            self.cache[key] = value
+            self.cache.move_to_end(key)
+        else:
+            temp = self.cache.popitem(last = False)
+            self.cache[key] = value
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
