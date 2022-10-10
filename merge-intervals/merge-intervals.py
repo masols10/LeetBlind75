79f31a8450d338df80fa86e9fcbb9d76e933b168
masols <manasa.solasa10@gmail.com>
2022-10-09 18:51:35 -0700

    Time: 381 ms, Memory: 19 MB - LeetHub

---
+++ merge-intervals/merge-intervals.py
@@ -0,0 +1,17 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda pair: pair[0])
+        output = [intervals[0]] #First interval
+
+        for start, end in intervals:
+            lastEnd = output[-1][1] # end value of most recently added interval output[-1]
+
+            if start <= lastEnd:
+                # merge
+                output[-1][1] = max(lastEnd, end) # we have to take the max of end values of intervals
+            else:
+                output.append([start, end])
+        return output
+    
+    
+      
