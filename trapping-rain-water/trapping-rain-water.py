4f37f0de26574ae6c528e57b44eff5abed53cb8b
masols <manasa.solasa10@gmail.com>
2022-10-09 19:30:42 -0700

    Time: 133 ms, Memory: 16 MB - LeetHub

---
+++ trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,3 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        return sum(accumulate(height, max)) + sum(accumulate(reversed(height), max)) - sum(height) - max(height) * len(height)
